d9c2252dd6ac43e3c80ee81e878dbd1dfcdbdb67
Développer backend <141308438+Styven-Manaja@users.noreply.github.com>
2024-05-21 20:48:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.set('views', path.join(__dirname, 'views'));
 
 /************************************************************/
 
